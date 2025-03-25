33afe3e63428c73ae29e9dc4b4b1db1319139490
engelryan <ryaneengel@gmail.com>
2025-03-25 19:43:48 -0300

    Adjusting some things

--- server/app/app/settings.py
+++ server/app/app/settings.py
@@ -130,4 +130,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#from .local_settings import *
+from .local_settings import *
--- server/app/aula/models/passport.py
+++ server/app/aula/models/passport.py
@@ -7,7 +7,7 @@ from .person import Person
 
 class Passport(BaseModel):
 
-    number = models.CharField( max_length=50, null=False, blank=False, help_text="...", verbose_name="Number")
+    number = models.CharField( max_length=15, null=False, blank=False, help_text="...", verbose_name="Number")
     issue_date = models.DateField(verbose_name="Issue Date")
     expiration = models.DateField(verbose_name="Expiration")
     owner = models.OneToOneField(Person, on_delete=models.CASCADE , primary_key=True,verbose_name="Owner")
